ae720dda943d073214f6289d049211faed1943ab
Manuelfjr <ferreira.jr.ufpb@gmail.com>
2021-04-01 11:20:15 -0300

    update:update setup to build package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="birt-sgd-manuelferreirajr", # Replace with your own username
+    version="0.0.1",
+    author="Manuel Ferreira Junior",
+    author_email="ferreira.jr.ufpb@gmail.com",
+    description=" Evaluation of clustering methods using Beta^3-IRT with descending gradient",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Manuelfjr/birt-sgd",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Source Code": "https://github.com/Manuelfjr/birt-sgd"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)

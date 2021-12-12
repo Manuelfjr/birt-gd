d8adbd7a5bb636d6493e108e1e1774a9e73c9075
Manuelfjr <ferreira.jr.ufpb@gmail.com>
2021-12-12 16:06:02 -0300

    update: new version | edit license in readme

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="birt-gd", # Replace with your own username
-    version="0.1.21",
+    version="0.1.22",
     author="Manuel Ferreira Junior",
     author_email="ferreira.jr.ufpb@gmail.com",
     description="BIRTGD is an implementation of Beta3-irt using gradient descent.",
     long_description=long_description,
-    license='GNU General Public License v3.0',
+    license='GNU General Public License v3 (GPLv3)',
     long_description_content_type="text/markdown",
     url="https://github.com/Manuelfjr/birt-gd",
     project_urls={
@@ -19,7 +19,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     install_requires=[
